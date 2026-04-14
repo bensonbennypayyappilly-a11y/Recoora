f699083ca510613114895c26531c36bcc621593f
Benson Benny <bensonbenny.payyappilly@gmail.com>
2026-04-14 16:32:20 +0530

    fix strpe connect

--- app/api/stripe/connect/callback/route.ts
+++ app/api/stripe/connect/callback/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
+import { createClient } from "@supabase/supabase-js";
+
 
 export async function GET(req: Request) {
   try {
@@ -16,12 +18,11 @@ export async function GET(req: Request) {
     // ✅ GET USER FIRST (FIXES YOUR ERROR)
     const cookieStore = await cookies();
 
-const supabase = createServerClient(
+
+
+const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  {
-    cookies: cookieStore,
-  }
+  process.env.SUPABASE_SERVICE_ROLE_KEY! // 🔥 IMPORTANT
 );
 
    // ✅ OPTIONAL: Validate state (basic protection)
